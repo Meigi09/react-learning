7dced8e757e9f31a9caa5943639d5863e4427fb9
Nshuti Meigi Mahoro <168594504+Meigi09@users.noreply.github.com>
2025-01-01 09:19:53 +0000

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './App.css'
+
 
 const App = () => {
   return (
-    <div>
-      <h2>Hello World</h2>
+    <div className='body'>
+      <h2>React Lessons</h2>
+      <ul>
+        <li>Super popular JS Library</li>
+        <li>React Has a pretty cool logo</li>
+        <li>Getting a job</li>
+      </ul>
     </div>
   )
 }
